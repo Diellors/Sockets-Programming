916fdec23a97d7e494390f3027cbf72187899464
Diellors <diellor.sinani@student.uni-pr.edu>
2026-04-19 15:17:02 +0200

    Implementimi i trajtuesit te fijeve te serverit per lidhjet e klienteve

--- Serveri1.cpp
+++ Serveri1.cpp
@@ -4,3 +4,19 @@
 #include <string>
 #include <cstring>
 
+#pragma comment(lib, "ws2_32.lib")
+
+#define PORT 8080
+
+DWORD WINAPI handleClient(LPVOID param)
+{
+    SOCKET clientSocket = (SOCKET)param;
+    char buffer[1024];
+
+    // read role
+    int bytes = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
+    if (bytes <= 0) {
+        closesocket(clientSocket);
+        return 0;
+    }
+}
