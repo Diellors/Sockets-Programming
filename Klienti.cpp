812c2d5e2b95eca1823ed90be0048181d71bbe79
arditaimeri1 <ardita.imeri3@student.uni-pr.edu>
2026-04-19 11:16:38 +0200

    ndryshimet ne kod

--- Klienti.cpp
+++ Klienti.cpp
@@ -11,3 +11,27 @@
 
 const char* SERVER_IP = "127.0.0.1";
 const int PORT = 8080;
+
+DWORD WINAPI receiveMessages(LPVOID param)
+{
+    SOCKET sock = (SOCKET)param;
+    char buffer[4096];
+
+    while (true)
+    {
+        int bytes = recv(sock, buffer, sizeof(buffer) - 1, 0);
+
+        if (bytes <= 0)
+        {
+            std::cout << "\n[!] Server disconnected.\n";
+            break;
+        }
+
+        buffer[bytes] = '\0';
+        std::cout << "\n[Server]: " << buffer << "\n> ";
+        std::cout.flush();
+    }
+
+    return 0;
+}
+
