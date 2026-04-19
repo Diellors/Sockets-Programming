2ecf4c85df0de420c5ec14c6aa11cfbdc0775d53
arditaimeri1 <ardita.imeri3@student.uni-pr.edu>
2026-04-19 11:12:45 +0200

    Klienti pjesa e pare

--- Klienti.cpp
+++ Klienti.cpp
@@ -1,1 +1,13 @@
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
 
+#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <string>
+
+#pragma comment(lib, "ws2_32.lib")
+
+const char* SERVER_IP = "127.0.0.1";
+const int PORT = 8080;
